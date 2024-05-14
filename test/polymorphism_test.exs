c905170c408ca3bf85dcffcc9168cc8e7acd968a
Daniel Newman <contact@lifeofdan.me>
2024-05-14 22:14:17 +1000

    Add additional test
    Add test to show creating a relationship on polymorphic resources.

--- test/polymorphism_test.exs
+++ test/polymorphism_test.exs
@@ -1,6 +1,6 @@
 defmodule AshPostgres.PolymorphismTest do
   use AshPostgres.RepoCase, async: false
-  alias AshPostgres.Test.{Post, Rating}
+  alias AshPostgres.Test.{Post, Rating, Hashtag, HashtagAssignment}
 
   require Ash.Query
 
@@ -25,5 +25,20 @@ defmodule AshPostgres.PolymorphismTest do
              |> Ash.Query.load(:ratings)
              |> Ash.read_one!()
              |> Map.get(:ratings)
+  end
+
+  @tag :focus
+  test "you can create relationship on polymorphic resource" do
+    hashtag =
+      Hashtag
+      |> Ash.Changeset.for_create(:create, %{label: "foobar"})
+      |> Ash.create!()
+
+    post = Post |> Ash.Changeset.for_create(:create) |> Ash.create!()
+
+    HashtagAssignment
+    |> Ash.Changeset.for_create(:create, %{resource_id: post.id})
+    |> Ash.Changeset.manage_relationship(:hashtag, hashtag, type: :append_and_remove)
+    |> Ash.create!()
   end
 end
